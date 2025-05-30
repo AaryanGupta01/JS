481d1164d7ae61d408cef9defe381893a1967fb8
AaryanGupta01 <aaryang0108@gmail.com>
2025-05-30 17:06:22 +0530

    Conversion of Datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Invalid!!") we are using nodejs not browser so doesn't work here
+
+/* 
+number => limit till 2 to the power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value not assigned
+symbol => unique
+*/
+
+console.log(typeof("Aaryan"))
+console.log(typeof(null)) // object
