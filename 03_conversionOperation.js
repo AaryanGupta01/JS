9aa1f64c25ec224ff13a38f581d7487bbf443e1f
AaryanGupta01 <aaryang0108@gmail.com>
2025-05-30 17:03:24 +0530

    Conversion of Datatypes

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -16,3 +16,8 @@ console.log(boolean_login);
 
 // empty string => false
 // string => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
